ca45129d130b94ceefb8e632e3e23f33fe9137e6
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 12:48:36 -0300

    Recapitulação

---
+++ Recapitulacao 2 exec 3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao 
+Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em 
+ordem crescente ou decrescente. */
+
+string[] numero = Console.ReadLine().Split(' ');
+int a = int.Parse(numero[0]);
+int b = int.Parse(numero[1]);
+
+if(a % b == 0 || b % a == 0)
+{
+    Console.WriteLine("São multiplos");
+}
+else
+{
+    Console.WriteLine("Não são mutiplos");
+}
